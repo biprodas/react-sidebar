b5a7a40eb2a4214c0c216d2d7747cc3adbf62710
Biprodas <biprodas.cse@gmail.com>
2020-01-02 01:02:39 +0600

    layout setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  state = {
+    leftOpen: true,
+    rightOpen: true,
+  }
+
+  toggleSidebar = (event) => {
+    let key = `${event.currentTarget.parentNode.id}Open`;
+    this.setState({ [key]: !this.state[key] });
+  }
+
+  render() {
+    let leftOpen = this.state.leftOpen ? 'open' : 'closed';
+    let rightOpen = this.state.rightOpen ? 'open' : 'closed';
+
+    return (
+      <div id='layout'>
+        <div id='left' className={leftOpen} >
+          <div className='icon' onClick={this.toggleSidebar}>
+            &equiv;
+          </div>
+          <div className={`sidebar ${leftOpen}`} >
+            <div className='header'>
+              <h3 className='title'>
+                Left header
+              </h3>
+            </div>
+            <div className='content'>
+              <h3>Left content</h3>
+              <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.              </p>
+            </div>
+          </div>
+        </div>
+
+        <div id='main'>
+          <div className='header'>
+            <h3 className={`title ${'left-' + leftOpen} ${'right-' + rightOpen}`}>
+                Main header
+            </h3>
+          </div>
+          <div className='content'>
+            <h3>Main content</h3><br/>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.            </p><br/>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.            </p><br/>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.            </p>
+          </div>
+        </div>
+
+        <div id='right' className={rightOpen} >
+          <div className='icon' onClick={this.toggleSidebar} >
+            &equiv;
+          </div>
+          <div className={`sidebar ${rightOpen}`} >
+            <div className='header'>
+              <h3 className='title'>
+                Right header
+              </h3>
+            </div>
+            <div className='content'>
+                <h3>Right content</h3><br/>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.                </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
